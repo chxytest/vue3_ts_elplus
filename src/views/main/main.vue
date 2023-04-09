09bc2fc4ff7b7511ffba1ce60f0274a7d164d4ff
chxiaoyu <chxiaoyu@chxiaoyudeMacBook-Air.local>
2023-04-09 17:49:27 +0800

    解决部分main页面无法显示的问题

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -11,7 +11,9 @@
         </el-header>
 
         <el-main class="page-content">
-          <router-view></router-view>
+          <div class="page-info">
+            <router-view></router-view>
+          </div>
         </el-main>
 
         <el-footer>Footer</el-footer>
@@ -60,6 +62,11 @@ export default defineComponent({
 
 .page-content {
   height: calc(100% - 48px);
+
+  .page-info {
+    background-color: #fff;
+    border-radius: 5px;
+  }
 }
 
 .el-header,
---
+++ src/views/main/system/user/user.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="user">
+    <div class="content">user page</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'user',
+  components: {},
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
